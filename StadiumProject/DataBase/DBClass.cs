d459949976b95f0bf4f09ac3203444174cbcc14e
ashwagMohsen <79316100+ashwagMohsen@users.noreply.github.com>
2023-02-11 12:26:39 +0300

    Add files via upload

---
+++ StadiumProject/DataBase/DBClass.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace StadiumProject
+{
+    class DBClass
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["StadiumProject"].ConnectionString;
+        public static SqlParameter CreateParameter(string name, object value)
+        {
+            return new SqlParameter(name, value);
+        }
+
+        static SqlCommand CreateCommand(string query, params SqlParameter[] prmArray)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+       
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                if (!query.Contains(" "))
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                if (prmArray.Length > 0)
+                    sqlCommand.Parameters.AddRange(prmArray);
+
+                sqlConnection.Open();
+
+                return sqlCommand;
+         
+        }
+
+        public static int ExcuteNonQuery(string query, params SqlParameter[] prmArray)
+        {
+            SqlCommand sqlCommand = CreateCommand(query,prmArray);
+
+            int affectedRows = sqlCommand.ExecuteNonQuery();
+
+            sqlCommand.Connection.Close();
+
+            return affectedRows;
+        }
+
+        public static object ExcuteScalar(string query, params SqlParameter[] prmArray)
+        {
+            SqlCommand sqlCommand = CreateCommand(query, prmArray);
+
+            object value = sqlCommand.ExecuteScalar();
+
+            sqlCommand.Connection.Close();
+
+            return value;
+        }
+
+        public static SqlDataReader ExcuteReader(string query, params SqlParameter[] prmArray)
+        {
+            using (SqlCommand sqlCommand = CreateCommand(query, prmArray))
+            {
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                return sqlDataReader;
+            }
+        }
+        public static DataTable ExcuteDataTable(string query, params SqlParameter[] prmArray)
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connectionString);
+           
+            if (!query.Contains(" "))
+                sqlDataAdapter.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            if (prmArray.Length > 0)
+                sqlDataAdapter.SelectCommand.Parameters.AddRange(prmArray);
+
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            return dt;
+        }
+  
+        public static DataSet ExcuteDataSet(string query, params SqlParameter[] prmArray)
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connectionString);
+
+            if (!query.Contains(" "))
+                sqlDataAdapter.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            if (prmArray.Length > 0)
+                sqlDataAdapter.SelectCommand.Parameters.AddRange(prmArray);
+
+            DataSet ds = new DataSet();
+            sqlDataAdapter.Fill(ds);
+
+            return ds;
+        }
+    }
+}
